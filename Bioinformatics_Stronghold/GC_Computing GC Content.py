022bcae8674b625769c562cf57bd5c0ce74b8416
wg-yoon <oknmj101@gmail.com>
2020-12-19 22:40:19 +0900

    Computing GC Content

---
+++ Bioinformatics_Stronghold/GC_Computing GC Content.py
@@ -0,0 +1,16 @@
+#
+# Fasta format으로 주어짐.
+# 가장 높은 GC content를 가진 ID와 GC content(%) 를 반환하면 됨
+
+def GC(fasta):
+    from Bio import SeqIO
+    parser = SeqIO.parse(fasta, format='fasta')
+    max_dic = {'ID' : None, 'GC':0}
+    for record in parser:
+        seq = record.seq
+        GC  = round((seq.count('G') + seq.count('C')) / len(seq) * 100 , 6)
+        if GC > max_dic['GC']:
+            max_dic['GC'] = GC
+            max_dic['ID'] = record.id
+        
+    return max_dic
