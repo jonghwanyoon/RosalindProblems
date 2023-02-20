d21603a0c1a6283eebc19c83cbca49381c6d64cc
wg-yoon <oknmj101@gmail.com>
2023-02-20 21:04:26 +0900

    BA3E

--- Bioinformatics_Textbook_Track/BA3E_Construct the De Bruijn Graph of a Collection of k-mers.py
+++ Bioinformatics_Textbook_Track/BA3E_Construct the De Bruijn Graph of a Collection of k-mers.py
@@ -1,14 +1,52 @@
 #
 # [BA3E Construct the De Bruijn Graph of a Collection of k-mers]
 #
+# 간단한 방법은 dictionary를 이용하면 BA3E로 표현 가능하다.
+#
+
+
+from collections import defaultdict
 
 def BA3E(Patterns):
-    dic={}
+    dic=defaultdict(list)
     for p in Patterns:
-        sub1 = p[:-1]
-        sub2 = p[1:]
-        if sub1 not in dic:
-            dic[sub1] = []
-        dic[sub1].append(sub2)
+        dic[p[:-1]].append(p[1:])
     for k in sorted(list(dic.keys())):
         print('{} -> {}'.format(k, ','.join(sorted(dic[k]))))
+
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.child = []
+
+    def addChild(self, node):
+        self.child.append(node)
+
+    def extendChilds(self, node_list):
+        self.child.extend(node_list)
+
+
+def BA3E_2(Patterns):
+    # Composition Graph
+    pool = []
+    for p in Patterns:
+        prefix = Node(p[:-1])
+        suffix = Node(p[1:])
+        prefix.addChild(suffix)
+        pool.append(prefix)
+    
+    # Path Graph
+    dic = {}
+    for node in pool:
+        prefix = node.value
+        if prefix not in dic:
+            dic[prefix] = node
+        else:
+            dic[prefix].extendChilds(node.child)
+
+    for kmer, node in dic.items():
+        prefix = node.value # equal with kmer
+        suffix_list = [child.value for child in node.child]
+        print(prefix + " -> " + ",".join(suffix_list) + "\n")
