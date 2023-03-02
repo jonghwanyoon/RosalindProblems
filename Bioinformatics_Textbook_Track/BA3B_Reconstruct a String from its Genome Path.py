7edfb6e5bcbcc7e14a4f4fe33795bcfde726da9a
wg-yoon <oknmj101@gmail.com>
2023-03-02 22:32:41 +0900

    230302

--- Bioinformatics_Textbook_Track/BA3B_Reconstruct a String from its Genome Path.py
+++ Bioinformatics_Textbook_Track/BA3B_Reconstruct a String from its Genome Path.py
@@ -1,6 +1,11 @@
 #
 # [BA3B Reconstruct a String from its Genome Path]
+# 
+# 문제는 K-mer로 된 서열이 n개 주어지며 i의 (1, K) 글자는 i+1의 (0, K-1)과 일치함.
+# 모두 나란히 이어져 있음을 알고 있다면 BA3B로 쉽게 해결이 된다.
 #
+# 하지만, Genome Path로 String을 재구축 하는 것이 목적이고, 실제에서는 이런 경우는 거의 없다.
+# BA3C 이후로 관련 문제가 계속해서 나타난다.
 
 def BA3B(DNA_list):
     return DNA_list[0] + ''.join([DNA_list[i][-1] for i in range(1, len(DNA_list))])
---
+++ Bioinformatics_Textbook_Track/BA3F_Find an Eulerian Cycle in a Graph.py
@@ -0,0 +1,13 @@
+
+#
+# [Find an Eulerian Cycle in a Graph]
+# https://rosalind.info/problems/ba3f/
+# 오일러 (Eulerian) 순환 경로 찾기
+#
+# adjacency list가 텍스트로 주어진다.
+# 오일러 경로는 edge를 단 한번만 건널 수 있다.
+# 오일러 순환 경로는, 모든 균형을 이룬 (indgree == outdegree), strongly connected graph이다.
+#
+
+def BA3F(patterns):
+    pass
